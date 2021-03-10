e538a46aa654554359353fb9c2ea353fb05d930a
Tibor17 <tibordigana@apache.org>
2021-03-10 12:02:06 +0100

    Updated SpringBoot to the latest Version 2.4.3

--- spring-boot/src/main/java/com/geowarin/config/JerseyConfig.java
+++ spring-boot/src/main/java/com/geowarin/config/JerseyConfig.java
@@ -1,18 +1,21 @@
 package com.geowarin.config;
 
 import com.geowarin.rest.RestResource;
-import org.glassfish.jersey.filter.LoggingFilter;
+import org.glassfish.jersey.logging.LoggingFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
 
-import javax.ws.rs.ApplicationPath;
+import java.util.logging.Logger;
+
+import static java.util.logging.Level.INFO;
+import static org.glassfish.jersey.logging.LoggingFeature.DEFAULT_LOGGER_NAME;
+import static org.glassfish.jersey.logging.LoggingFeature.Verbosity.PAYLOAD_ANY;
 
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
         register(RequestContextFilter.class);
-        //packages("com.geowarin.rest");
         register(RestResource.class);
-        register(LoggingFilter.class);
+        register(new LoggingFeature(Logger.getLogger(DEFAULT_LOGGER_NAME), INFO, PAYLOAD_ANY, 10_000));
     }
 }
